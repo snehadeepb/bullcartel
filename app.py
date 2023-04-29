fbdcb25c115e14e08cf73aed3fbe47d42ca9a245
Snehadeep  Barwade <115915017+snehadeepb@users.noreply.github.com>
2023-04-29 17:54:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,27 +7,26 @@ from time import gmtime, strftime
 from IPython.display import clear_output 
 import matplotlib.pyplot as plt
 from pytz import timezone 
-import json
+from deta import Deta 
+from st_aggrid import AgGrid
 
 def get_data():
-#     a=None
-#     last_prices=None
-    a =(nse_fno("BANKNIFTY"))
-    last_prices=round(nse_quote_ltp("BANKNIFTY"))
-    while a==None and last_prices ==None:
-        a=(nse_fno("BANKNIFTY"))
-        last_prices=round(nse_quote_ltp("BANKNIFTY"))
-#         try:
-            
-#         except json.decoder.JSONDecodeError:
-#             print('The file contains invalid JSON')  # 👇️ this runs
-#             time.sleep(1*60)
+    a=(nse_fno("BANKNIFTY"))
+#     last_prices=round(nse_quote_ltp("BANKNIFTY"))
+    global open1,last_prices,high,low
+    price=(nse_quote_meta("BANKNIFTY","latest","Fut"))
+    open1=price['openPrice']
+    last_prices=round(price['lastPrice'])
+    high=price['highPrice']
+    low=price['lowPrice']
+    print(open1,high,low,last_prices)
+    
     exp=list(set(a['expiryDates']))
     exp.sort(key = lambda date: datetime.strptime(date, '%d-%b-%Y')) 
     if last_prices%100>50:
         x=(last_prices-last_prices%100+100)
         strike=[x-200,x-100,x,x+100,x+200]
-    elif last_prices%100<=50:
+    elif last_prices%100<50:
         x=(last_prices-last_prices%100)
         strike=[x-200,x-100,x,x+100,x+200]
     d={'call change op':[],
@@ -52,6 +51,7 @@ def get_data():
                 d['put vwap'].append(i['marketDeptOrderBook']['tradeInfo']['vmap'])
 
     out=pd.json_normalize(d)
+    
     out=out.explode(list(out.columns)).reset_index(drop = True)
     out.fillna(0,inplace=True)
     x=out.astype(float).round(2)
@@ -63,9 +63,16 @@ def get_info(dataset):
     pcr= dataset['put change op'].sum()/dataset['call change op'].sum()
     cal_per= dataset['% change op'].mean()
     put_per= dataset['% change op put'].mean()
-    new_row={'time':datetime.now(timezone("Asia/Kolkata")).strftime('%I.%M %p'),'value':value, 'pcr':round(pcr,2), 'cal_per':round(cal_per,2), 'put_per':round(put_per,2)}
+    new_row={'time':datetime.now(timezone("Asia/Kolkata")).strftime('%I.%M %p'),'value':value, 'pcr':round(pcr,2), 'cal_per':round(cal_per,2), 'put_per':round(put_per,2),'open': round(open1),'high':round(high),'low':round(low),'close':round(last_prices)}
     df = df.append(new_row,ignore_index=True, verify_integrity=False, sort=None)
-    return df  
+    deta_key="d0iqnepq4nn_BgRSHUYswKQEwYxUJEFnFgH4FTfwm8EH"
+    deta = Deta(deta_key)
+    db = deta.Base("bullcartal1")
+    def insert_user(row):
+         return db.put(row)
+    insert_user(new_row)
+    return df 
+
 def ploting():
         try:
             global final
@@ -73,11 +80,19 @@ def ploting():
              df = pd.DataFrame(columns=['value', 'pcr', 'cal_per','put_per'])
         dataset= get_data()
         main= get_info(dataset)
-        final =final.append(main,ignore_index=True, verify_integrity=False, sort=None)
+        main1=main[['value', 'pcr', 'cal_per','put_per','time']]
+        final =final.append(main1,ignore_index=True, verify_integrity=False, sort=None)
+#         deta_key="d0iqnepq4nn_BgRSHUYswKQEwYxUJEFnFgH4FTfwm8EH"
+#         deta = Deta(deta_key)
+#         db = deta.Base("bullcartal1")
+#         def insert_user(row):
+#              return db.put( row)
+#         insert_user(row)
+        
         return dataset,final
 
 final = pd.DataFrame(columns=['value', 'pcr', 'cal_per','put_per','time'])
-    
+
 
 if __name__=='__main__':
     
@@ -93,12 +108,14 @@ if __name__=='__main__':
              PUT % INCREASE MEANS MARKET GOES UP
              """)    
     while True:
+        current_time=datetime.now(timezone("Asia/Kolkata")).strftime('%I.%M %p')
         dataset,final=ploting()
         p1=st.empty()
         p2=st.empty()
         p3=st.empty()
         p1.dataframe(dataset.style.highlight_max(['% change op put','% change op'],axis=0)) #Column hightlight 
-#         p2.dataframe(final.style.highlight_max(['cal_per','put_per'],axis=1)) # row highlight
+#         p2.dataframe(final.style.highlight_max(['cal_per','put_per'],axis=1,)) # row highlight
+#         p2.AgGrid(final,height=500,fit_columns_on_grid_load=True)
         p2.write(final[:100])
         fig, ax = plt.subplots(figsize=(6, 2)) 
         ax.plot(final['time'],final['pcr'])
@@ -110,17 +127,3 @@ if __name__=='__main__':
         p2.empty()
         p3.empty()
 
-#             start_time='09.14 AM'
-#             stop_time='06.05 PM'
-#             current_time=datetime.now(timezone("Asia/Kolkata")).strftime('%I.%M %p')
-#             today = datetime.today().strftime('%w') # DAY WITH NO AND SAT AND SUNDAY IS 60
-#             if today not in '60':
-#                 if current_time>stop_time:
-# #                     current_time=datetime.now(timezone("Asia/Kolkata")).strftime('%I.%M %p')
-                                          
-# #                 else:
-# #             else:
-#                        break #time.sleep(15*60) #saturday and sunday all the time sleep mode that is the calculated (how to go else part at which tiem  then monday start time )
-#         except:
-#             st.write("Error message is:",'some kind problem restart 10 min')
-#             time.sleep(10*60)
