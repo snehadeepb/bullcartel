564bf34df0b0786f6569f938a65a5bfa6ed2e9dc
Snehadeep  Barwade <115915017+snehadeepb@users.noreply.github.com>
2023-05-05 11:26:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,8 +123,8 @@ if __name__=='__main__':
 # #         final=np.array(final,column=['value', 'pcr', 'cal_per','put_per','time'])
 #         p2.dataframe(final.style.highlight_max(['cal_per','put_per'],axis=1,)) # row highlight
         p1.dataframe(dataset.style.highlight_max(['% change op put','% change op'],axis=0)) #Column hightlight 
-#         p2.dataframe(final.style.highlight_max(['cal_per','put_per'],axis=1)) # row highlight
-        p2.write(final[:100])
+        p2.dataframe(final) # row highlight
+#         p2.write(final[:100])
 #         p2.AgGrid(final,height=500,fit_columns_on_grid_load=True)
 #         p2.write(final[:100])
         fig, ax = plt.subplots(figsize=(6, 2)) 
