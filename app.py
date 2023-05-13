682be1f485b738a01f42e5fadade4a13803299e4
Snehadeep  Barwade <115915017+snehadeepb@users.noreply.github.com>
2023-05-13 12:06:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ import yfinance as yf
 
 def get_data():
     a=(nse_fno("BANKNIFTY"))
+    a = json.dumps(a)
 #     last_prices=round(nse_quote_ltp("BANKNIFTY"))
     global open1,last_prices,high,low,strike
     
