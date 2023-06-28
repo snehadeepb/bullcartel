c6dd07af5ea6846ff91afd93ded6d24142d6faa2
Snehadeep  Barwade <115915017+snehadeepb@users.noreply.github.com>
2023-06-28 09:31:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def get_data():
                 if  ll < lmax:
                     dict_list[lname] += [padel] * (lmax - ll)
             return dict_list
-    out = pad_dict_list(d, 0)
+            out = pad_dict_list(d, 0)
     x=out.astype(float).round(2)
     x.sort_values("strike", axis = 0, ascending = True,inplace = True)
     return x
